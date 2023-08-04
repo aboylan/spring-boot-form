94e5dc1eae4a6bf3828a5178afcfae7095a1f5fe
Alex Boylan <aboylan@users.noreply.github.com>
2023-08-04 13:48:56 -0600

    Anadiendo componente service de Pais

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.support.SessionStatus;
 import com.bolsadeideas.springboot.form.app.editors.NombreMayusculaEditor;
 import com.bolsadeideas.springboot.form.app.models.domain.Pais;
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import com.bolsadeideas.springboot.form.app.services.PaisService;
 import com.bolsadeideas.springboot.form.app.validation.UsuarioValidador;
 
 import jakarta.validation.Valid;
@@ -33,6 +34,9 @@ public class FormController {
 
 	@Autowired
 	private UsuarioValidador validador;
+
+	@Autowired
+	private PaisService paisService;
 
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
@@ -46,9 +50,7 @@ public class FormController {
 
 	@ModelAttribute("listaPaises")
 	public List<Pais> listaPaises() {
-		return Arrays.asList(new Pais(1, "ES", "Espana"), new Pais(2, "MX", "Mexico"), new Pais(3, "CL", "Chile"),
-				new Pais(4, "AR", "Argentina"), new Pais(5, "PE", "Peru"), new Pais(6, "CO", "Colombia"),
-				new Pais(7, "VE", "Venezuela"));
+		return paisService.listar();
 	}
 
 	@ModelAttribute("paises")
