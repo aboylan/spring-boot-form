e0769432467d2f75abb9bb24fa42f570bf4ae7f5
Alex Boylan <aboylan@users.noreply.github.com>
2023-08-04 09:32:49 -0600

    Formateando fechas con @InitBinder y registrando un CustomDateEditor

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -14,7 +14,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class Usuario {
@@ -48,7 +47,7 @@ public class Usuario {
 
 	@NotNull
 	@Future
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	//@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date fechaNacimiento;
 
 	public String getIdentificador() {
